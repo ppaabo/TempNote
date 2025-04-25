333fd69f3a24c1f4395f38e928a0503c62ad16a9
ppaabo <89841937+ppaabo@users.noreply.github.com>
2025-04-25 19:33:43 +0300

    Add & configure pg_cron for the database, setup job to delete expired messages

---
+++ db/02-init-pg_cron.sql
@@ -0,0 +1,5 @@
+-- 02-init-pg_cron.sql
+-- Initialize in the postgres database (where cron runs)
+\c postgres;
+CREATE EXTENSION IF NOT EXISTS pg_cron;
+GRANT USAGE ON SCHEMA cron TO CURRENT_USER;
---
+++ db/03-init-cron-jobs.sql
@@ -0,0 +1,10 @@
+-- 03-init-cron-jobs.sql
+\c postgres;
+
+-- Run every hour (xx:00)
+SELECT cron.schedule_in_database(
+  'cleanup-expired-messages',
+  '0 * * * *',
+  'DELETE FROM messages WHERE expires_at < NOW()',
+  'flask_db'
+);
